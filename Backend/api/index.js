3d33f4e98467611883f4c72e3be41bd46a9fa3e1
Osama-Alhaji <ausamaalhaji2@gmail.com>
2020-07-18 20:29:00 +0300

    Changes are made

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import sequelize from './config/database';
-import User from "../Entities/user"
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -11,12 +10,7 @@ sequelize
   .catch((err) => console.log('Error: ' + err));
 const port = process.env.PORT || 8000;
 // when a random route is inputed
-app.get("/user",async(req,res)=>{
 
-  const users = await User.findAll()
-  console.log(users)
-  
-  })
 app.get('*', (req, res) =>
   res.status(200).send({
     message: 'Welcome to this API.',
